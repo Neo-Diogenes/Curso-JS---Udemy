07a0461aa5cbea765d3fb25bd56edd1e2ad7b4fc
Neo-Diogenes <klebs123.ss@gmail.com>
2022-07-15 20:16:55 -0300

    Atualização

--- js/Aulas/aula62-exercicio3.js
+++ js/Aulas/aula62-exercicio3.js
@@ -1,8 +1,8 @@
 function fizzBuzz (numero) {
     if (typeof numero !== 'number') return numero;
-    if (n % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
-    if (n % 3 === 0) return 'Fizz';
-    if (n % 5 === 0) return 'Buzz';
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
     
     return numero;
 }
